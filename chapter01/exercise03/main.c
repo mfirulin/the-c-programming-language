7c2c2b8d402113fcfcf5d01e900cc6c478cc6ff8
Mikhail Firulin <mfirulin@gmail.com>
2018-12-01 20:57:29 +0300

    Create main.c

---
+++ chapter01/exercise03/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+    
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    fahr = lower;
+    
+    printf("FAHR CELSIUS\n");
+    
+    while(fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr += step;
+    }
+
+    return EXIT_SUCCESS;
+}

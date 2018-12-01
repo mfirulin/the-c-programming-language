8022b8679cc637c0e823c9a314d3773ee4f1a7ba
Mikhail Firulin <mfirulin@gmail.com>
2018-12-01 22:03:49 +0300

    Rename chapter01/exercise03/main.c to chapter01/exercise04/main.c

---
+++ chapter01/exercise04/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+    
+    lower = -20;
+    upper = 160;
+    step = 20;
+    
+    celsius = lower;
+    
+    printf("CELSIUS  FAHR\n");
+    
+    while(celsius <= upper) {
+        fahr = (9.0 / 5.0) * celsius  + 32.0;
+        printf("%7.0f %5.1f\n", celsius, fahr);
+        celsius += step;
+    }
+
+    return EXIT_SUCCESS;
+}

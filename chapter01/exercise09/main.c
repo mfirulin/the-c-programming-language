59ef3e80ad10a8481519ff21a6d3aea220e17897
Mikhail Firulin <mfirulin@gmail.com>
2018-12-02 10:33:09 +0300

    Create main.c

---
+++ chapter01/exercise09/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c, s = 0;
+    
+    while ((c = getchar()) != EOF) {
+        if (c == ' ')
+            ++s;
+        else
+            s = 0;
+            
+        if (s <= 1)
+            putchar(c);
+    }
+
+    return EXIT_SUCCESS;
+}

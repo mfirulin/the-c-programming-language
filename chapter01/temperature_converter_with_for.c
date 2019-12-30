8ccc515e690baddd4f30992b84b2891790cfb572
Mikhail Firulin <mfirulin@gmail.com>
2019-12-30 13:01:23 +0300

    Update temperature_converter_with_for.c

--- chapter01/temperature_converter_with_for.c
+++ chapter01/temperature_converter_with_for.c
@@ -3,11 +3,13 @@
 
 int main()
 {
+    printf("FAHR CELS\n");
     for (int fahr = 0; fahr <= 300; fahr = fahr + 20)
         printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
         
     printf("\n");
     
+    printf("CELS FAHR\n");
     for (int cels = -20; cels <= 160; cels += 10)
         printf("%3d %6.1f\n", cels, (9.0 / 5.0) * cels + 32.0);
 

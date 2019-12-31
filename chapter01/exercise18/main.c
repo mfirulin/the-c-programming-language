524bbc959de01396b1d41d1a6bb999600f303b64
Mikhail Firulin <mfirulin@gmail.com>
2019-12-31 14:42:56 +0300

    Update main.c

--- chapter01/exercise18/main.c
+++ chapter01/exercise18/main.c
@@ -40,25 +40,25 @@ void rtrim(char line[])
 {
     int i = 0, j, nl = 0;
     
-    while (line[i])
+    while (line[i]) // find end of the line
         ++i;
         
-    if (i > 0) {
+    if (i > 0) { // we are on '\0' now, so if not empty line, step back
         --i;
         
         j = i;
-        while (i >= 0 && line[i] == '\n')
+        while (i >= 0 && line[i] == '\n') // trim all '\n' if several
             --i;
         
-        if (j != i)
+        if (j != i) // there was at least one '\n' so remember this
             nl = 1;
         
-        while (i >= 0 && (line[i] == ' ' || line[i] == '\t'))
+        while (i >= 0 && (line[i] == ' ' || line[i] == '\t')) // trim all spaces and tabs
             --i;
         
         i++;
         
-        if (nl)
+        if (nl) // if there was '\n' return one back
             line[i++] = '\n';
         
         line[i] = '\0';
